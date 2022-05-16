b6fe1911a89aa029556ee8f215079a702c664f6b
Pedro Queiroz <pccql@cin.ufpe.br>
2022-05-15 21:14:04 -0300

    feat: add group.txt

--- helpers/miniaturePic.py
+++ helpers/miniaturePic.py
@@ -7,10 +7,6 @@ class MiniaturePic:
         self.text_area = text_area
 
     def add_image(self, type_of_file, name_of_file):
-        all_files = [name_of_file, "files_for_test/ursinho-polar.jpeg",
-                     "files_for_test/urso-polar-grande.png"]
-        # file = all_files[type_of_file]
-
         fileName = name_of_file.split('/')[-1]
         file = f"files_for_test/{fileName}"
         image = Image.open(file.replace('$', ''))
